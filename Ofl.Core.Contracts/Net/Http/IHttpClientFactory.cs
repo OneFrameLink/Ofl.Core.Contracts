075317ceda7203c00d61a17b5b1cd582f36cd699
casperOne <casperOne@caspershouse.com>
2017-02-23 08:39:58 -0500

    Initial commit.

---
+++ Ofl.Core.Contracts/Net/Http/IHttpClientFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading;
+
+namespace Ofl.Core.Net.Http
+{
+    public interface IHttpClientFactory
+    {
+        HttpClient CreateAsync(CancellationToken cancellationToken);
+
+        HttpClient CreateAsync(HttpMessageHandler handler, CancellationToken cancellationToken);
+
+        HttpClient CreateAsync(HttpMessageHandler handler, bool disposeHandler, CancellationToken cancellationToken);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/vy4897okth5eqm72?svg=true)](https://ci.appveyor.com/project/OneFrameLink/ofl-core-contracts)
+
+# Ofl.Core.Contracts
+Abstractions used by the Ofl.Core project.
